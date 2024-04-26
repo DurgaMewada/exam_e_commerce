3005f0951aba7a31798ae999f93d8c1487541b9b
DurgaMewada <mewadadurga83@gmail.com>
2024-04-26 15:58:14 +0530

    CartScreen.dart

--- lib/Screens/CartScreen.dart
+++ lib/Screens/CartScreen.dart
@@ -58,7 +58,7 @@ body: Column(
               width: double.infinity,
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(10),
-                color: Colors.grey.shade300,
+                color: Colors.grey.shade200,
               ),
               child: Row(
                 children: [
@@ -71,10 +71,72 @@ body: Column(
                       ),
                       borderRadius: BorderRadius.circular(10),
                     ),
-                  )
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text('\$ 300',style: TextStyle(
+                        color: Colors.black,fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),),
+                      Text('Rolex Watch',style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.black
+                      ),)
+                    ],
+                  ),
+                  SizedBox(
+                    width: 80,
+                  ),
+                  Icon(Icons.delete,size: 30,color: Colors.black,)
+
                 ],
               ),
-            )
+            ),
+            SizedBox(height: 10,
+            ),
+            Container(
+              height: 100,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.grey.shade200,
+              ),
+              child: Row(
+                children: [
+                  Container(
+                    height: 90,
+                    width: 100,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage('asset/image/img4.png'),
+                      ),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text('\$ 500',style: TextStyle(
+                        color: Colors.black,fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),),
+                      Text('Rolex Watch',style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.black
+                      ),)
+                    ],
+                  ),
+                  SizedBox(
+                    width: 80,
+                  ),
+                  Icon(Icons.delete,size: 30,color: Colors.black,)
+
+                ],
+              ),
+            ),
           ],
         ),
       ),
